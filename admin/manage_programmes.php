7b5ef597dc0a8f12f67e8e55902cfdf79aace94d
Richard-dev10 <elsonpoudel777@gmail.com>
2026-03-12 19:01:35 +0100

    Add new programme form to manage programmes page

--- admin/manage_programmes.php
+++ admin/manage_programmes.php
@@ -9,6 +9,24 @@ if (!isset($_SESSION["admin"])) {
 ?>
 
 <h1>Manage Programmes</h1>
-<p>This page will be used to manage programmes.</p>
+
+<h2>Add New Programme</h2>
+
+<form method="POST">
+
+<label>Programme Name:</label>
+<input type="text" name="programme_name"><br><br>
+
+<label>Level:</label>
+<input type="text" name="level"><br><br>
+
+<label>Programme Leader:</label>
+<input type="text" name="leader"><br><br>
+
+<button type="submit">Add Programme</button>
+
+</form>
+
+<hr>
 
 <p><a href="dashboard.php">Back to Dashboard</a></p>
