5c77fe5f53ddddf966c41893af04af2564e86855
Richard-dev10 <elsonpoudel777@gmail.com>
2026-03-12 18:45:39 +0100

    Protect admin dashboard using session authentication

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,5 +1,11 @@
 <?php
 session_start();
+
+// if admin session does not exist, send user back to login page
+if (!isset($_SESSION["admin"])) {
+    header("Location: login.php");
+    exit();
+}
 ?>
 
 <h1>Admin Dashboard</h1>
