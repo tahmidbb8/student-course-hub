93c44869453b04bbfe6f1d7ea5769a540e1505f0
Richard-dev10 <elsonpoudel777@gmail.com>
2026-03-26 19:58:39 +0100

    Fix Modules table auto-increment issue and allow multiple module inserts

--- admin/manage_modules.php
+++ admin/manage_modules.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Manage Modules</title>
-    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="admin-style.css">
 </head>
 <body>
 
